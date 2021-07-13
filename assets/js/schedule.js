e74221337f9209d5a612a7b6693e88e4f0f51d5f
Sean <sdillon215@gmail.com>
2021-07-13 15:52:51 -0700

    handler addded to save btn click

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -15,8 +15,24 @@ $(".event-row").on("click", "p", function () {
 });
 
 //   when user clicks outside the textarea turns <textarea> back to <p> with added text
-  $(".event-row").on("blur", "textarea", function () {
-    // get the textareas current value/text
+//   $(".event-row").on("blur", "textarea", function () {
+//     // get the textareas current value/text
+//     var text = $(this)
+//       .val()
+//       .trim();
+//       console.log(text);
+
+//     // recreate p element
+//     var taskp = $("<p>")
+//       .addClass("form-control my-auto")
+//       .text(text);
+
+//     // replace textarea with p
+//     $(this).replaceWith(taskp);
+//     console.log(this);
+//   });
+
+$(".event-row button").click(function () {
     var text = $(this)
       .val()
       .trim();
@@ -29,9 +45,5 @@ $(".event-row").on("click", "p", function () {
 
     // replace textarea with p
     $(this).replaceWith(taskp);
-    console.log(this);
-  });
-
-$(".event-row button").click(function () {
     console.log("save button clicked");
 });
