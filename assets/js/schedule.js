b95dbcc61d71b6afa62d3573caef6848ce65d5f0
Sean <sdillon215@gmail.com>
2021-07-13 14:20:07 -0700

    blur

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -2,30 +2,36 @@ var tasks = {};
 // display date in header
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-// $(".list-group").on("click")
-
-$(".list-group").on("click", "p", function () {
+// when form is clicked turns into <p> into <textarea>
+$(".event-row").on("click", "p", function () {
     var text = $(this)
-      .text()
-      .trim();
+        .text()
+        .trim();
     var textInput = $("<textarea>")
-      .addClass("form-control my-auto")
-      .val(text);
+        .addClass("form-control my-auto new-event")
+        .val(text);
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
-  });
+});
 
-  $(".list-group").on("blur", "textarea", function () {
+//   when user clicks outside the textarea turns <textarea> back to <p> with added text
+  $(".event-row").on("blur", "textarea", function () {
     // get the textareas current value/text
     var text = $(this)
       .val()
       .trim();
-  
+      console.log(text);
+
     // recreate p element
     var taskp = $("<p>")
       .addClass("form-control my-auto")
       .text(text);
-  
+
     // replace textarea with p
     $(this).replaceWith(taskp);
+    console.log(this);
   });
+
+$(".event-row button").click(function () {
+    console.log("save button clicked");
+});
