38729cab3339f1dbdf296884304f209af5ee2b47
Sean <sdillon215@gmail.com>
2021-07-12 17:16:29 -0700

    add tasks

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,3 +1,31 @@
+var tasks = {};
 // display date in header
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
+// $(".list-group").on("click")
+
+$(".list-group").on("click", "p", function () {
+    var text = $(this)
+      .text()
+      .trim();
+    var textInput = $("<textarea>")
+      .addClass("form-control my-auto")
+      .val(text);
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+  });
+
+  $(".list-group").on("blur", "textarea", function () {
+    // get the textareas current value/text
+    var text = $(this)
+      .val()
+      .trim();
+  
+    // recreate p element
+    var taskp = $("<p>")
+      .addClass("form-control my-auto")
+      .text(text);
+  
+    // replace textarea with p
+    $(this).replaceWith(taskp);
+  });
