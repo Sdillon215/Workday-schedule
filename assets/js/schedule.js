858bf67a2e44d5bd9a9993b96781b12979b0130c
Sean <sdillon215@gmail.com>
2021-07-18 16:07:44 -0700

    for loop added still bugs

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,4 +1,5 @@
 // display date in header
+var NowMoment = moment();
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 
@@ -10,7 +11,7 @@ var storage = function (txtId, btnId, keyValue) {
     localStorage.setItem(keyValue, newEvent);
   });
   var savedEvent = localStorage.getItem(keyValue);
-  console.log(savedEvent);
+  $(txtId).val(savedEvent);
 };
 
 // passes parameters for each row to the storage function
@@ -18,8 +19,24 @@ storage("#9", "#btn1", "keyvalue1");
 storage("#10", "#btn2", "keyValue2");
 storage("#11", "#btn3", "keyValue3");
 storage("#12", "#btn4", "keyValue4");
-storage("#1", "#btn5", "keyValue5");
-storage("#2", "#btn6", "keyValue6");
-storage("#3", "#btn7", "keyValue7");
-storage("#4", "#btn8", "keyValue8");
-storage("#5", "#btn9", "keyValua9");
+storage("#13", "#btn5", "keyValue5");
+storage("#14", "#btn6", "keyValue6");
+storage("#15", "#btn7", "keyValue7");
+storage("#16", "#btn8", "keyValue8");
+storage("#17", "#btn9", "keyValua9");
+
+var eventText = $(".event-text")
+
+
+for (var i = 0; i < eventText.length; i++) {
+  var eventTextArea = $(eventText[i]);
+  let eventHour = parseInt(eventText.attr("id"));
+  console.log(eventHour);
+  let currentHour = NowMoment.format("H");
+  console.log(currentHour);
+  if (currentHour > eventHour) {
+    eventTextArea.addClass("past");
+  } else if (currentHour == eventHour) {
+    eventTextArea.addClass("present");
+  };
+};
