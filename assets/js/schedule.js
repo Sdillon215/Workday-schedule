602bf020f4bb52ecda10036f7c749d9660c9437a
Sean <sdillon215@gmail.com>
2021-07-18 13:51:23 -0700

    js start over...

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -1,78 +1,13 @@
-var tasksArr = [];
 // display date in header
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
-// when form is clicked turns into <p> into <textarea>
-$(".event-row").on("click", "p", function () {
-  var text = $(this)
-    .text()
-    .trim();
-  var textInput = $("<textarea>")
-    .addClass("form-control my-auto new-event")
-    .val(text);
-  $(this).replaceWith(textInput);
-  textInput.trigger("focus");
-});
 
+// create function to set and get items from localStorage
+var storage = function (txtId, btnId, value) {
+  $(btnId).on("click", function() {
+    var newValue = $(txtId).val();
+    console.log(newValue);
+  })
+}
 
-$(".event-row button").click(function () {
-  var txtArea = $(this)
-    .closest(".event-row")
-    .children(".list-group")
-    .children("textarea")
-    .val()
-    .trim();
-
-  // get parents id
-  var status = $(".new-event")
-    .closest(".list-group")
-    .attr("id");
-
-  // recreate p element
-  var taskp = $("<p id=" + status + ">")
-    .addClass("form-control my-auto")
-    .text(txtArea);
-
-  var taskId = taskp.attr("id");
-  var taskText = taskp.text();
-  console.log(taskId);
-  console.log(taskText);
-
-  tasksArr.push({
-    id: taskId,
-    text: taskText
-  });
-
-  saveLocal();
-  loadTasks();
-  // replace textarea with p
-  $("textarea").replaceWith(taskp);
-});
-
-var saveLocal = function () {
-  localStorage.setItem("tasksArr", JSON.stringify(tasksArr));
-};
-
-var loadTasks = function (tasksArr) {
-  tasksArr = JSON.parse(localStorage.getItem(tasksArr));
-  if (!tasksArr) {
-    tasksArr = {};
-  }
-  console.log(tasksArr);
-};
-
-
-
-loadTasks();
-
-var date = moment();
-
-// var auditEvent = function (taskId) {
-//   var currentTime = parseInt(date.format("H"));
-//   console.log(taskId);
-//   console.log(currentTime);
-// };
-
-
-
-// auditEvent();
+storage("#9", "#btn1", "value");
