60895a38cff090633b65dde8cdab77de654a2df3
Sean <sdillon215@gmail.com>
2021-07-18 16:11:30 -0700

    textId changed to textClass

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -4,26 +4,26 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 
 // create function to set and get items from localStorage
-var storage = function (txtId, btnId, keyValue) {
+var storage = function (txtClass, btnId, keyValue) {
   $(btnId).on("click", function() {
-    var newEvent = $(txtId).val();
+    var newEvent = $(txtClass).val();
     console.log(newEvent);
     localStorage.setItem(keyValue, newEvent);
   });
   var savedEvent = localStorage.getItem(keyValue);
-  $(txtId).val(savedEvent);
+  $(txtClass).val(savedEvent);
 };
 
 // passes parameters for each row to the storage function
-storage("#9", "#btn1", "keyvalue1");
-storage("#10", "#btn2", "keyValue2");
-storage("#11", "#btn3", "keyValue3");
-storage("#12", "#btn4", "keyValue4");
-storage("#13", "#btn5", "keyValue5");
-storage("#14", "#btn6", "keyValue6");
-storage("#15", "#btn7", "keyValue7");
-storage("#16", "#btn8", "keyValue8");
-storage("#17", "#btn9", "keyValua9");
+storage(".text1", "#btn1", "keyvalue1");
+storage(".text2", "#btn2", "keyValue2");
+storage(".text3", "#btn3", "keyValue3");
+storage(".text4", "#btn4", "keyValue4");
+storage(".text5", "#btn5", "keyValue5");
+storage(".text6", "#btn6", "keyValue6");
+storage(".text7", "#btn7", "keyValue7");
+storage(".text8", "#btn8", "keyValue8");
+storage(".text9", "#btn9", "keyValua9");
 
 var eventText = $(".event-text")
 
