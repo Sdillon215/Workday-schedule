a940ee336c022a4c60fcb4b4907c32f82b5c4801
Sean <sdillon215@gmail.com>
2021-07-18 17:16:13 -0700

    comments added

--- assets/js/schedule.js
+++ assets/js/schedule.js
@@ -6,16 +6,17 @@ $("#currentDay").text(moment().format("dddd, MMMM Do YYYY"));
 
 // create function to set and get items from localStorage
 var storage = function (txtClass, btnId, keyValue) {
+  // on save btn click txtClass value is set to localStorage
   $(btnId).on("click", function () {
     var newEvent = $(txtClass).val();
-    console.log(newEvent);
     localStorage.setItem(keyValue, newEvent);
   });
+  // gets items from localStorage and populates events
   var savedEvent = localStorage.getItem(keyValue);
   $(txtClass).val(savedEvent);
 };
 
-// passes parameters for each row to the storage function
+// passes parameters for each event to the storage function
 storage(".text1", "#btn1", "keyvalue1");
 storage(".text2", "#btn2", "keyValue2");
 storage(".text3", "#btn3", "keyValue3");
@@ -26,12 +27,15 @@ storage(".text7", "#btn7", "keyValue7");
 storage(".text8", "#btn8", "keyValue8");
 storage(".text9", "#btn9", "keyValua9");
 
-
+// checks task time vs actual time and changes color accordingly
 var auditTask = function () {
+  // for loop to iterate through events
   for (var i = 0; i < eventText.length; i++) {
-
+    // assigns eventText at current index to eventTextArea
     var eventTextArea = $(eventText[i]);
+    // assigns the eventTextArea id to eventHour and changes to integer
     var eventHour = parseInt(eventTextArea.attr("id"));
+    // uses moment to get current hour assigns to currentHour
     var currentHour = NowMoment.format("H");
 
     if (currentHour > eventHour) {
@@ -45,9 +49,11 @@ var auditTask = function () {
   };
 };
 
+// calls auditTask function every 15 mins
 setInterval(function () {
   auditTask();
-}, (1000 * 60) * 30);
+}, (1000 * 60) * 15);
 
+// call auditTask on page load
 auditTask();
 
